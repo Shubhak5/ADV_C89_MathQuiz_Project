79ad09d5b3e8a65c0760096ade8b2cb0e83db999
Shubhak5 <83910260+Shubhak5@users.noreply.github.com>
2021-09-11 23:35:18 +0530

    ADV_C92_Project
    Updated game_page.js with check() function

--- game_page.js
+++ game_page.js
@@ -6,12 +6,19 @@ player2_name = localStorage.getItem("player2");
 player1_score = 0;
 player2_score = 0;
 
+question_turn = "Player1";
+answer_turn = "Player2";
+
 //Display the player names in the page
 document.getElementById("player1_name").innerHTML = player1_name + " : ";
 document.getElementById("player2_name").innerHTML = player2_name + " : ";
 
 document.getElementById("player1_score").innerHTML = player1_score;
 document.getElementById("player2_score").innerHTML = player2_score;
+
+//Set up the initial turns
+document.getElementById("spnQTurn").innerHTML = player1_name;
+document.getElementById("spnATurn").innerHTML = player2_name;
 
 function send() {
     number1 = document.getElementById("number1").value;
@@ -27,4 +34,37 @@ function send() {
 
     document.getElementById("number1").value = "";
     document.getElementById("number2").value = "";
+}
+
+function check() {
+    get_answer = document.getElementById("inptAns").value;
+
+    //Check if answer is correct and update score value
+    if (get_answer == actualAns) {
+        if (answer_turn == "Player1") {
+            player1_score += 1;
+            document.getElementById("player1_score").innerHTML = player1_score;
+        } else {
+            player2_score += 1;
+            document.getElementById("player2_score").innerHTML = player2_score;
+        }
+    }
+
+    //Set Question Turn and Answer turn
+    if (question_turn == "Player1") {
+        question_turn = "Player2";
+        answer_turn = "Player1";
+
+        document.getElementById("spnQTurn").innerHTML = player2_name;
+        document.getElementById("spnATurn").innerHTML = player1_name;
+    } else {
+        question_turn = "Player1";
+        answer_turn = "Player2";
+
+        document.getElementById("spnQTurn").innerHTML = player1_name;
+        document.getElementById("spnATurn").innerHTML = player2_name;
+    }
+
+    document.getElementById("number1").innerHTML = "";
+    document.getElementById("number2").innerHTML = "";
 }
